63d8fae5127a3baef1fd2ecb25988bd48ce392ea
Charles Smith <cas275@pitt.edu>
2015-11-02 21:44:40 -0500

    added a helper method to find the location of an address in memory (if it exists)

--- vmsim.c
+++ vmsim.c
@@ -1,10 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define OFFSET 12
+
+
+int num_frames;
+int alg;
+
+int loc_in_mem(int mem[], int address);
+
+
+
 int main(int argc, char ** argv){
-  printf("hello world!");
+
+
+  int mem[num_frames]; 
+  
+  
+  
+  
 
 
 
   return 0;
+}
+
+/**
+ * returns index if found, -1 otherwise
+ *
+ */
+int loc_in_mem(int mem[], int address){
+
+  int found = -1;
+  int i;
+  address= address >>OFFSET;  //shift away the offset, we don't want it
+  for(i=0; i<num_frames; i++){
+    if(address = mem[i] >>OFFSET){
+      found = i;
+      break;
+    }
+  }
+  return found;
+
+
 }
